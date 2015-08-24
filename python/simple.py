742824fe9fbb69dc2b0004c6360eafa0cc4fb32e
Aurelien Moreau <aurelien.moreau@angus.ai>
2015-08-24 11:17:57 +0200

    Initial commit. Push the two first examples.

---
+++ python/simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import angus
+import json
+
+conn = angus.connect()
+service = conn.services.get_service('age_and_gender_estimation', version=1)
+job = service.process({'image': open("./macgyver.jpg")})
+print json.dumps(job.result, indent=4)
