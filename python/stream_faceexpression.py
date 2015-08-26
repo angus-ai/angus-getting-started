a3d81cc489f6c550b84133cbdbd2ba20ef56df3f
Gwennael Gate <gwennaelgate@gmail.com>
2015-08-26 13:21:51 +0200

    re-enabling session in faceexpression

--- python/stream_faceexpression.py
+++ python/stream_faceexpression.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
 
     conn = angus.connect()
     service = conn.services.get_service('face_expression_estimation', 1)
-    #service.enable_session()
+    service.enable_session()
     
     while(cap.isOpened()):
         ret, frame = cap.read()
@@ -55,7 +55,7 @@ if __name__ == '__main__':
                     exp_sorted.reverse()
                     max_exp = exp_sorted[0]                    
 
-                    if max_exp[1] > 0.5:
+                    if max_exp[1] > 0.7:
                         cv2.putText(frame,
                                     str(max_exp[0]),
                                     (int(roi[0]), int(roi[1])),
