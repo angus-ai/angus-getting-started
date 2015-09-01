c62e0d1635ea24abce785f57ffdf67c9b42441e3
gwennaelgate <gwennael.gate@angus.ai>
2015-09-01 18:11:34 +0200

    moving images in a specific folder

--- python/simple.py
+++ python/simple.py
@@ -4,5 +4,5 @@ import json
 
 conn = angus.connect()
 service = conn.services.get_service('age_and_gender_estimation', version=1)
-job = service.process({'image': open("./macgyver.jpg")})
+job = service.process({'image': open("./images/macgyver.jpg")})
 print json.dumps(job.result, indent=4)
