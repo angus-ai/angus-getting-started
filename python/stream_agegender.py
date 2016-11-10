9e532e42bdb92cd7f176b13c2c53dcaad1eba022
Gwennael Gate <gwennael.gate@angus.ai>
2016-11-10 18:44:58 +0100

    fixing agegender example

--- python/stream_agegender.py
+++ python/stream_agegender.py
@@ -4,10 +4,12 @@ import cv2
 import angus
 import numpy as np
 
-if __name__ == '__main__':    
+if __name__ == '__main__':
     ### Web cam index might be different from 0 on your setup.
-    stream_index = 0 
+    stream_index = 0
     cap = cv2.VideoCapture(stream_index)
+    cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 480)
 
     if not cap.isOpened():
         print "Cannot open stream of index " + str(stream_index)
@@ -19,7 +21,7 @@ if __name__ == '__main__':
     service = conn.services.get_service("age_and_gender_estimation", version=1)
     service.enable_session()
 
-    while(cap.isOpened()):
+    while cap.isOpened():
         ret, frame = cap.read()
         if not frame == None:
 
@@ -31,15 +33,15 @@ if __name__ == '__main__':
             res = job.result
 
             if res['nb_faces'] > 0:
-                for i in range(0,res['nb_faces']):
+                for i in range(0, res['nb_faces']):
                     roi = res['faces'][i]['roi']
-                    cv2.rectangle(frame, (int(roi[0]), int(roi[1])), 
-                                         (int(roi[0] + roi[2]), int(roi[1] + roi[3])), 
+                    cv2.rectangle(frame, (int(roi[0]), int(roi[1])),
+                                         (int(roi[0] + roi[2]), int(roi[1] + roi[3])),
                                          (0,255,0))
                     age = res['faces'][i]['age']
                     gender = res['faces'][i]['gender']
-                    cv2.putText(frame, "(age, gender) = (" + '%.1f'%age + ", " + str(gender) + ")", 
-                                (int(roi[0]), int(roi[1])), cv2.FONT_HERSHEY_SIMPLEX, 
+                    cv2.putText(frame, "(age, gender) = (" + '%.1f'%age + ", " + str(gender) + ")",
+                                (int(roi[0]), int(roi[1])), cv2.FONT_HERSHEY_SIMPLEX,
                                 0.8, (255, 255, 255))
 
 
