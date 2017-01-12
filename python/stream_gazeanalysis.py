63846bc9699125b897b225fa21a90929457c798e
Gwennael Gate <gwennael.gate@angus.ai>
2017-01-12 15:35:28 +0100

    Upgrading gaze analysis

--- python/stream_gazeanalysis.py
+++ python/stream_gazeanalysis.py
@@ -1,83 +1,88 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import StringIO
-import pprint
-from math import sin, cos
+from math import cos, sin
 import cv2
 import numpy as np
 import angus
 
-LINETH = 3
+def main(stream_index):
+    camera = cv2.VideoCapture(0)
+    camera.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 640);
+    camera.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 480);
+    camera.set(cv2.cv.CV_CAP_PROP_FPS, 10)
 
-if __name__ == '__main__':
+    if not camera.isOpened():
+        print("Cannot open stream of index {}".format(stream_index))
+        exit(1)
 
-    ### To grab the host computer web cam instead of a given file, try:
-    cap = cv2.VideoCapture(0)
-    cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 640)
-    cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 480)
+    print("Input stream is of resolution: {} x {}".format(camera.get(3), camera.get(4)))
 
-    print "Input stream is of resolution: " + str(cap.get(3)) + " x " + str(cap.get(4))
     conn = angus.connect()
     service = conn.services.get_service('gaze_analysis', 1)
     service.enable_session()
 
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if frame != None:
-            ### angus.ai computer vision services require gray images right now.
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            ret, buff = cv2.imencode(".png", gray)
-            buff = StringIO.StringIO(np.array(buff).tostring())
+    while camera.isOpened():
+        ret, frame = camera.read()
+        if not ret:
+            break
 
-            job = service.process({"image": buff})
-            res = job.result
+        ### angus.ai computer vision services require gray images right now.
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        ret, buff = cv2.imencode(".jpg", gray, [cv2.IMWRITE_JPEG_QUALITY, 80])
+        buff = StringIO.StringIO(np.array(buff).tostring())
 
-            print "---------- Raw answer from Angus.ai -----------"
-            pprint.pprint(res)
-            print "-----------------------------------------------"
+        job = service.process({"image": buff})
+        res = job.result
+
+        for face in res['faces']:
+            x, y, dx, dy = map(int, face['roi'])
+
+            nose = face['nose']
+            nose = (nose[0], nose[1])
+
+            eyel = face['eye_left']
+            eyel = (eyel[0], eyel[1])
+            eyer = face['eye_right']
+            eyer = (eyer[0], eyer[1])
+
+            psi = face['head_roll']
+            theta = - face['head_yaw']
+            phi = face['head_pitch']
+
+            ### head orientation
+            length = 150
+            xvec = int(length*(sin(phi)*sin(psi) - cos(phi)*sin(theta)*cos(psi)))
+            yvec = int(- length*(sin(phi)*cos(psi) - cos(phi)*sin(theta)*sin(psi)))
+            cv2.line(frame, nose, (nose[0]+xvec, nose[1]+yvec), (0, 140, 255), 3)
+
+            psi = 0
+            theta = - face['gaze_yaw']
+            phi = face['gaze_pitch']
+
+            ### gaze orientation
+            length = 150
+            xvec = int(length*(sin(phi)*sin(psi) - cos(phi)*sin(theta)*cos(psi)))
+            yvec = int(- length*(sin(phi)*cos(psi) - cos(phi)*sin(theta)*sin(psi)))
+            cv2.line(frame, eyel, (eyel[0]+xvec, eyel[1]+yvec), (0, 140, 0), 3)
+
+            xvec = int(length*(sin(phi)*sin(psi) - cos(phi)*sin(theta)*cos(psi)))
+            yvec = int(- length*(sin(phi)*cos(psi) - cos(phi)*sin(theta)*sin(psi)))
+            cv2.line(frame, eyer, (eyer[0]+xvec, eyer[1]+yvec), (0, 140, 0), 3)
 
 
-            if res['nb_faces'] > 0:
-                for h in res['faces']:
-                    nose = h['nose']
-                    nose = (nose[0], nose[1])
-                    eyel = h['eye_left']
-                    eyel = (eyel[0], eyel[1])
-                    eyer = h['eye_right']
-                    eyer = (eyer[0], eyer[1])
+        cv2.imshow('original', frame)
 
-                    psi = h['head_roll']
-                    theta = - h['head_yaw']
-                    phi = h['head_pitch']
-
-                    length = 150
-                    xvec = int(length*(sin(phi)*sin(psi) - cos(phi)*sin(theta)*cos(psi)))
-                    yvec = int(- length*(sin(phi)*cos(psi) - cos(phi)*sin(theta)*sin(psi)))
-                    cv2.line(frame, nose, (nose[0]+xvec, nose[1]+yvec), (0, 140, 255), LINETH)
-
-                    psi = 0
-                    theta = - h['gaze_yaw']
-                    phi = h['gaze_pitch']
-
-                    length = 150
-                    xvec = int(length*(sin(phi)*sin(psi) - cos(phi)*sin(theta)*cos(psi)))
-                    yvec = int(- length*(sin(phi)*cos(psi) - cos(phi)*sin(theta)*sin(psi)))
-                    cv2.line(frame, eyel, (eyel[0]+xvec, eyel[1]+yvec), (0, 140, 0), LINETH)
-
-                    xvec = int(length*(sin(phi)*sin(psi) - cos(phi)*sin(theta)*cos(psi)))
-                    yvec = int(- length*(sin(phi)*cos(psi) - cos(phi)*sin(theta)*sin(psi)))
-                    cv2.line(frame, eyer, (eyer[0]+xvec, eyer[1]+yvec), (0, 140, 0), LINETH)
-
-
-            cv2.imshow('Gaze Analysis', frame)
-
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        else:
-            print "None image read"
-
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
     ### Disabling session on the server
     service.disable_session()
 
-    cap.release()
+    camera.release()
     cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    ### Web cam index might be different from 0 on your setup.
+    ### To grab a given video file instead of the host computer cam, try:
+    ### main("/path/to/myvideo.avi")
+    main(0)
